c1bf67438c1aad43293aa9f434c2ff68646634e3
Tipe123 <monyemangene.2@gmail.com>
2021-06-15 15:59:30 +0200

    done with login

--- members/views.py
+++ members/views.py
@@ -13,6 +13,7 @@ class UserRegisterView(generic.CreateView):
 	template_name = 'Registration/register.html'
 	redirect_authenticated_user = True
 	success_url = reverse_lazy('index')
+	
 	def form_valid(self,form):
 		user = form.save()
 		if user is not None:
